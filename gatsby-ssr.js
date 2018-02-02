1fb08d9836e5c1f6ee82de52514fdee6d6f78246
Hunter M. Wells <hunter.m.wells@gmail.com>
2018-02-02 13:11:06 -0800

    hail mary with gatsby-ssr

---
+++ gatsby-ssr.js
@@ -0,0 +1,64 @@
+import React from "react";
+import { renderToString } from "react-dom/server";
+import ApolloClient, { createNetworkInterface } from "apollo-client";
+import { ApolloProvider, getDataFromTree } from "react-apollo";
+import { HttpLink } from "apollo-link-http";
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import fetch from 'node-fetch';
+
+// function to generate hydrated state for client side Apollo
+function makeApolloState(ssrClient) {
+  const state = { apollo: ssrClient.getInitialState() }
+  // appends apollo state to the global client window object
+  return (
+    <script
+      dangerouslySetInnerHTML={{
+        __html: `window.__APOLLO_STATE__=${JSON.stringify(state).replace(
+          /</g,
+          `\\u003c`
+        )};`
+      }}
+    />
+  );
+}
+
+const client = new ApolloClient({
+  ssrMode: true,
+  link: new HttpLink({
+    uri: 'https://api.graph.cool/simple/v1/cjd56vfu6212z0183clcoouaa',
+    fetch,
+  }),
+  cache: new InMemoryCache()
+});
+
+exports.replaceRenderer = ({
+  bodyComponent,
+  replaceBodyHTMLString,
+  setHeadComponents
+}) =>
+  new Promise((resolve) => {
+    // bodyComponent is the entire React component tree for the page
+    /*
+      to remove styled components:
+      1. remove const sheet
+      2. remove StyleSheetManager
+      3. remove sheet.getStyleElement from setHeadComponents array
+    */
+
+    const ApolloQueries = (
+      <ApolloProvider client={client}>
+
+          {bodyComponent}
+
+      </ApolloProvider>
+    );
+    // getDataFromTree walks ApolloQueries tree for all Apollo GQL queries
+    // It runs the queries and mutates client object
+    getDataFromTree(ApolloQueries).then(() => {
+      // renders ApolloQueries to string and then inserts it into the page
+      replaceBodyHTMLString(renderToString(ApolloQueries))
+      // sets head components with styled components and apollo state
+      setHeadComponents([makeApolloState(client)])
+      resolve();
+    })
+  })
